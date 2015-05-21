f2ef3dca20cc986cac7e62486cb1ffa9604cbd36
Fran Gonzalez <francisco.gonzalez.garcia@bisdn.de>
2015-05-21 16:00:20 +0200

    Implemented handling of service start and stop (remove container and restore initial system conf), dynamic configuration of dnsmasq and hostapd, dynamic execution depending on the wireless interface

--- README.md
+++ README.md
@@ -2,10 +2,17 @@
 
 This script configures an Ubuntu-based system to act as a WiFi access point. The AP runs inside a docker container.
 
-The script should be run as sudo. The first time the script is run, it will pull the docker image from fgg89/ubuntu-ap repository. This docker image is based on baseimage (Please visit https://github.com/phusion/baseimage-docker for more info). The image contains dnsmasq and hostapd. 
+The script should be run as sudo. The first time the script is executed, it will pull the docker image from fgg89/ubuntu-ap repository. This docker image is based on baseimage (Please visit https://github.com/phusion/baseimage-docker for more info). The image contains the programs dnsmasq and hostapd. Their respective configuration files are generated on the fly and mounted in the docker container.
 
-dnsmasq configuration
-=====================
+## Usage
+
+```
+./docker_ap.sh interface start|stop
+```
+
+## Example of configuration files
+
+### dnsmasq configuration
 
 ```
 no-resolv
@@ -15,11 +22,10 @@ no-dhcp-interface=lo
 dhcp-range=192.168.7.20,192.168.7.254,255.255.255.0,12h
 ```
 
-hostapd configuration
-=====================
+### hostapd configuration
 
 ```
-sid=DockerAP
+ssid=DockerAP
 interface=wlan5
 hw_mode=g
 channel=1
@@ -34,4 +40,4 @@ logger_stdout=-1
 logger_stdout_level=2
 ```
 
-NOTE: In this case, wlan5 is the WiFi interface to be used.
+The ssid, wpa_passphrase, interface and subnetwork are initially hardcoded as variables in the script.
--- docker_ap.sh
+++ docker_ap.sh
@@ -5,53 +5,126 @@
 #author		 :Fran Gonzalez
 #date            :20150520
 #version         :0.1    
-#usage		 :bash docker_ap.sh
+#usage		 :bash docker_ap.sh start|stop
 #notes           :Install iptables (with nat kernel module) and docker to use this
 #                 script.
 #bash_version    :
-#==============================================================================all
+#==============================================================================all 
 
-echo " ___          _               _   ___"  
-echo "|   \ ___  __| |_____ _ _    /_\ | _ \\"
-echo "| |) / _ \/ _| / / -_) '_|  / _ \|  _/"
-echo "|___/\___/\__|_\_\___|_|   /_/ \_\_|  "
-echo ""
+### Variables
 
-### Local variables
-
-IFACE="wlan5"
-SUBNET="192.168.7.0"
+SSID="DockerAP"
+PASSPHRASE="dockerap123"
+SUBNET="192.168.7"
 IP_AP="192.168.7.1"
 NETMASK="/24"
+PATHSCRIPT=`pwd`
+NAME="ap-container"
 
-### Check if network-manager is running
-
-if ps aux | grep -v grep | grep network-manager > /dev/null
+IFACE=$1
+if [ "$IFACE" == "" ]
 then
-    echo [+] Network manager is running
-    echo [+] Turning nmcli wifi off
-    # Fix hostapd bug in Ubuntu 14.04
-    nmcli nm wifi off
-else
-    echo [+] Network manager is stopped
+    IFACE="wlan5"
 fi
 
-rfkill unblock wifi
-ifconfig $IFACE up
+init () {
 
-### Assign IP to the wifi interface
-echo [+] Configuring $IFACE with IP address $IP_AP 
-ip addr flush dev $IFACE
-ip addr add $IP_AP$NETMASK dev $IFACE
+    echo " ___          _               _   ___"  
+    echo "|   \ ___  __| |_____ _ _    /_\ | _ \\"
+    echo "| |) / _ \/ _| / / -_) '_|  / _ \|  _/"
+    echo "|___/\___/\__|_\_\___|_|   /_/ \_\_|  "
+    echo ""
 
-### iptables rules for NAT
-echo [+] Adding natting rule to iptables
-iptables -t nat -A POSTROUTING -s $SUBNET$NETMASK ! -d $SUBNET$NETMASK -j MASQUERADE
 
-### Enable IP forwarding
-echo [+] Enabling IP forwarding
-echo 1 > /proc/sys/net/ipv4/ip_forward
+    ### Check if dnsmasq is running
+    if ps aux | grep -v grep | grep dnsmasq > /dev/null
+    then
+        echo [+] dnsmasq is running
+        echo [+] Turning dnsmasq off
+        killall dnsmasq
+    else
+        echo [+] dnsmasq is stopped
+    fi
 
-### Start the docker (baseimage)
-echo [+] Starting the docker container
-sudo docker run --rm -t -i --net=host --privileged fgg89/ubuntu-ap /sbin/my_init -- bash -l
+    ### Check if network-manager is running
+    if ps aux | grep -v grep | grep network-manager > /dev/null
+    then
+        echo [+] Network manager is running
+        echo [+] Turning nmcli wifi off
+        # Fix hostapd bug in Ubuntu 14.04
+        nmcli nm wifi off
+    else
+        echo [+] Network manager is stopped
+    fi
+
+    rfkill unblock wifi
+    ifconfig $IFACE up
+
+    ### Assign IP to the wifi interface
+    echo [+] Configuring $IFACE with IP address $IP_AP 
+    ip addr flush dev $IFACE
+    ip addr add $IP_AP$NETMASK dev $IFACE
+
+    ### iptables rules for NAT
+    echo [+] Adding natting rule to iptables
+    iptables -t nat -A POSTROUTING -s $SUBNET.0$NETMASK ! -d $SUBNET.0$NETMASK -j MASQUERADE
+
+    ### Enable IP forwarding
+    echo [+] Enabling IP forwarding 
+    echo 1 > /proc/sys/net/ipv4/ip_forward
+
+### Generating hostapd conf file
+cat <<EOF > $PATHSCRIPT/hostapd.conf
+ssid=$SSID
+interface=$IFACE
+hw_mode=g
+channel=1
+wpa=2
+wpa_passphrase=$PASSPHRASE
+wpa_key_mgmt=WPA-PSK
+logger_syslog=-1
+logger_syslog_level=2
+logger_stdout=-1
+logger_stdout_level=2
+EOF
+
+### Generating dnsmasq conf file
+cat <<EOF > $PATHSCRIPT/dnsmasq.conf
+no-resolv 
+server=8.8.8.8
+interface=lo,$IFACE
+no-dhcp-interface=lo
+dhcp-range=$SUBNET.20,$SUBNET.254,255.255.255.0,12h
+EOF
+
+}
+
+service_start () { 
+    echo [+] Starting the docker container
+    ## --rm, remove this flag if you want the container to remain after stopping it
+    docker run --rm -t -i --name $NAME --net=host --privileged -v $PATHSCRIPT/hostapd.conf:/etc/hostapd/hostapd.conf -v $PATHSCRIPT/dnsmasq.conf:/etc/dnsmasq.conf fgg89/ubuntu-ap /sbin/my_init -- bash -l
+
+}
+
+service_stop () { 
+    echo [+] Stopping the docker container and reverting system configuration
+    docker stop $NAME
+#    docker rm $NAME
+    ip addr del $IP_AP$NETMASK dev $IFACE
+    iptables -t nat -D POSTROUTING -s $SUBNET.0$NETMASK ! -d $SUBNET.0$NETMASK -j MASQUERADE
+    echo 0 > /proc/sys/net/ipv4/ip_forward
+    rm $PATHSCRIPT/hostapd.conf
+    rm $PATHSCRIPT/dnsmasq.conf
+}
+
+if [ "$2" == "start" ]
+then
+    init
+    service_start
+elif [ "$2" == "stop" ]
+then
+    service_stop
+else
+    echo "Please enter a valid argument"
+fi
+
